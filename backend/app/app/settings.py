bffc4db23bd76d598cf5999a159d4a0f6742505a
Geunwoo Park <qkr0677@gmail.com>
2021-06-13 18:53:15 +0900

    [fix/back] 로컬/ 배포 디비 연동 분리

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -76,13 +76,33 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'teameet',
+            'USER': 'admin',
+            'PASSWORD': 'YRnD1zMUxzCiRhQJ0BHm',
+            'HOST': 'database-1.cluster-cdihlcooafvr.ap-northeast-2.rds.amazonaws.com',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+            }
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
