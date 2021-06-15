f49d461730634ab06b636d144d41233adc318074
Geunwoo Park <qkr0677@gmail.com>
2021-06-15 13:43:39 +0900

    [fix/back] 도커없이 장고 돌려보기 위해 세팅 진행

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-mp4m&^+r36$507ucoyibbown0(@%h@fxvjm%gtl@+4k6((w%cs'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
