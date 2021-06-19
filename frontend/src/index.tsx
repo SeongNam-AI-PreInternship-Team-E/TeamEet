b35cbfd475b6df56fdaec7554e3d4f8cb0c19e14
ww8007 <wshmin@naver.com>
2021-06-19 21:07:45 +0900

    [feat/front] add_HashRouter, Middleware... 디버깅을 위한 composeWithDevTools 후의 build를 위한 라우터 변경 리듀서 이용을 위해 store 생성

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer, { rootSaga } from './modules';
+import createSagaMiddleware from 'redux-saga';
+import { HashRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+); // 스토어를 만듭니다.
 
+sagaMiddleware.run(rootSaga);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
