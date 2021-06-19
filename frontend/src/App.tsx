ea1150f31c2373f11c3288e97b692aadc237231e
ww8007 <wshmin@naver.com>
2021-06-19 23:27:45 +0900

    [feat/front] add_SampleContainer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import * as React from 'react';
+import SampleContainer from './components/SampleContainer';
+type Props = {};
+const App = (props: Props) => {
+  return <SampleContainer />;
+};
 
 export default App;
