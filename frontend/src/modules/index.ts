185a7eea6d0af8b3ed5f5bded3a2c090fb823493
김유신 <84851734+lamerry0213@users.noreply.github.com>
2021-06-24 01:55:42 +0900

    [feat/front] add_time_reducer 루트 리듀서에 추가

--- frontend/src/modules/index.ts
+++ frontend/src/modules/index.ts
@@ -1,11 +1,19 @@
-import { combineReducers } from 'redux';
-import loading from './loading';
-import sample, { sampleSaga } from './sample';
-import calendar from './calendar';
-import { all } from 'redux-saga/effects';
-import user, { userSaga } from './user';
-import auth, { authSaga } from './auth';
-const rootReducer = combineReducers({ loading, sample, calendar, user, auth });
+import { combineReducers } from "redux";
+import loading from "./loading";
+import sample, { sampleSaga } from "./sample";
+import calendar from "./calendar";
+import { all } from "redux-saga/effects";
+import user, { userSaga } from "./user";
+import auth, { authSaga } from "./auth";
+import time from "./time";
+const rootReducer = combineReducers({
+  loading,
+  sample,
+  calendar,
+  user,
+  auth,
+  time,
+});
 
 export default rootReducer;
 
