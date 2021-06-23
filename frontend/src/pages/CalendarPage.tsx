01cda98f51318f6eff3c610e4ff60058b40ee574
ww8007 <wshmin@naver.com>
2021-06-23 21:30:04 +0900

    [feat/front] add_CalendarPage 켈린더 켄테이너를 렌더링 하는 페이지 생성

---
+++ frontend/src/pages/CalendarPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { CalendarContainer } from '../components/CalendarContainer';
+interface Props {}
+
+export const CalendarPage = (props: Props) => {
+  return <CalendarContainer />;
+};
