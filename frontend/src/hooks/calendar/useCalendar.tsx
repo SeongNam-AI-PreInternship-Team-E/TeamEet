bfce27cc12129b23aac05004e61cf75e4e467bf6
ww8007 <wshmin@naver.com>
2021-06-24 20:42:11 +0900

    [feat/fonrt] add_modal, title_input 모달 창 추가와 제목에 대한 인풋 박스 생성

--- frontend/src/hooks/calendar/useCalendar.tsx
+++ frontend/src/hooks/calendar/useCalendar.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
+import Button from '../../lib/styles/Button';
 import styled from 'styled-components';
+import dayjs from 'dayjs';
+import 'dayjs/plugin/timezone';
+import 'dayjs/locale/ko';
+import 'dayjs/plugin/utc';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   addDays,
@@ -10,26 +15,39 @@ import {
   changeStarDay,
   dragMonth,
   clickMonth,
+  setDay,
+  setInitialDate,
+  addTitle,
 } from '../../modules/calendar';
 import { RootState } from '../../modules';
 
 import CalendarPresent from './CalendarPresent';
+import Modal, { useModal } from '../common/useModal';
 
 interface Props {}
 
 export function useCalendar() {
-  const { Days, weekOfDay, month } = useSelector((state: RootState) => ({
+  const { Days, weekOfDay, month, title } = useSelector((state: RootState) => ({
     Days: state.calendar.Days,
     weekOfDay: state.calendar.weekOfDay,
     month: state.calendar.month,
+    title: state.calendar.title,
   }));
   const [isDown, onChangeDown] = useState({
     drag: false,
     key: 99999,
   });
+  const timezone = require('dayjs/plugin/timezone');
+  const utc = require('dayjs/plugin/utc');
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(setDay(dayjs().tz('Asia/Seoul').locale('ko')));
     dispatch(addDays());
+    return () => {
+      dispatch(setInitialDate());
+    };
   }, [dispatch]);
   const onClickPrev = () => {
     dispatch(prevMonth());
@@ -51,6 +69,9 @@ export function useCalendar() {
   const onClickMonth = (id: number) => {
     dispatch(clickMonth(id));
   };
+  const onChangeTitle = (word: string) => {
+    dispatch(addTitle(word));
+  };
   return {
     onClickPrev,
     onClickNext,
@@ -63,6 +84,8 @@ export function useCalendar() {
     isDown,
     onChangeDown,
     onClickMonth,
+    onChangeTitle,
+    title,
   };
 }
 
@@ -74,8 +97,61 @@ export interface day {
 }
 const CalendarWrapper = styled.div`
   width: 100%;
-  height: 100%;
-  border: 1px solid black;
+  height: 80%;
+  padding: 1rem;
+`;
+
+const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  h4 {
+    width: 10%;
+  }
+  h5 {
+    width: 5%;
+  }
+`;
+
+const Title = styled.input`
+  padding-top: 5rem;
+  display: flex;
+  width: 40%;
+  font-size: 1.5rem;
+  border: none;
+  border-bottom: 1px solid black;
+  padding-bottom: 1rem;
+
+  outline: none;
+  min-width: 17rem;
+  height: 6rem;
+  text-align: center;
+  background-color: transparent;
+  color: black;
+  ::placeholder {
+    padding-left: 0.5rem;
+    color: #bbbbbb;
+  }
+  &:focus {
+    ::placeholder {
+      color: white;
+    }
+    color: black;
+  }
+  & + & {
+    margin-top: 2.5rem;
+  }
+`;
+
+const ButtonClick = styled(Button)`
+  display: flex;
+  height: 3rem;
+  cursor: pointer;
+  align-items: center;
+  width: 10%;
+  min-width: 4rem;
+  justify-content: center;
 `;
 
 const CalendarContainer = styled.div`
@@ -87,18 +163,33 @@ const CalendarContainer = styled.div`
 
 const Header = styled.div`
   display: flex;
-  flex-direction: column;
-  padding-left: 1.2rem;
 
-  h2 {
-    font-weight: 800;
-    font-size: 1.5rem;
-  }
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
   h3 {
-    display: flex;
-    flex-direction: row;
+    z-index: 999;
+    box-sizing: border-box;
+
     font-weight: 600;
     font-size: 1.2rem;
+    width: 60%;
+    display: flex;
+    justify-content: center;
+  }
+  h2 {
+    z-index: 1;
+    display: flex;
+    width: 10%;
+    font-weight: 800;
+    font-size: 1.5rem;
+    justify-content: center;
+    padding-left: 1rem;
+  }
+  h4 {
+    width: 5%;
   }
 `;
 
@@ -108,8 +199,7 @@ const Cal = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(7, 1fr);
-  width: auto;
-  height: auto;
+  box-sizing: border-box;
   text-align: center;
 `;
 
@@ -126,13 +216,32 @@ export const Calendar = (props: Props) => {
     onClickMonth,
     isDown,
     onChangeDown,
+    onChangeTitle,
+    title,
   } = useCalendar();
-
+  const { modal, onCancel, onConfirm, onNextClick } = useModal();
   return (
     <CalendarWrapper>
       <CalendarContainer>
+        <TitleContainer>
+          <h5 />
+          <h4 />
+          <h4 />
+          <Title
+            placeholder="모임명을 입력 하세요"
+            onChange={(e) => {
+              onChangeTitle(e.target.value);
+            }}
+          ></Title>
+          <h4 />
+          <h5 />
+          <ButtonClick cyan onClick={onNextClick}>
+            <div>생성</div>
+          </ButtonClick>
+        </TitleContainer>
         <Header>
-          <h2>{month.format('YYYY')}</h2>
+          <h2 />
+          <h4 />
           <h3>
             <AiFillCaretLeft
               onClick={() => {
@@ -150,6 +259,8 @@ export const Calendar = (props: Props) => {
               style={{ cursor: 'pointer' }}
             ></AiFillCaretRight>
           </h3>
+          <h4></h4>
+          <h2>{month.format('YYYY')}</h2>
         </Header>
         <Cal>
           {weekOfDay.map((week) => (
@@ -169,6 +280,7 @@ export const Calendar = (props: Props) => {
           ))}
         </Cal>
       </CalendarContainer>
+      <Modal visible={modal} onCancel={onCancel} onConfirm={onConfirm}></Modal>
     </CalendarWrapper>
   );
 };
