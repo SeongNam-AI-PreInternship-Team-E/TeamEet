2fd37fef7473d24d460b364d7b1623625ca7cb59
ww8007 <wshmin@naver.com>
2021-06-24 20:42:48 +0900

    [feat/front] add_useModal_hook 모달에 대한 처리를 하는 hook 추가

---
+++ frontend/src/hooks/common/useModal.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '../../lib/styles/Button';
+import UseHour from './UseHour';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../modules';
+
+interface Props {}
+
+export function useModal() {
+  const { title } = useSelector((state: RootState) => ({
+    title: state.calendar.title,
+  }));
+  const [modal, setModal] = useState(false);
+  const [start, setStart] = useState('');
+  const [end, setEnd] = useState('');
+
+  const onNextClick = () => {
+    if (title === '') {
+      alert('입력칸이 비어 있습니다.');
+      return;
+    }
+    setModal(true);
+  };
+  const onCancel = () => {
+    setModal(false);
+  };
+  const onConfirm = () => {
+    setModal(false);
+  };
+  const onSetStart = (time: string) => {
+    setStart(time);
+  };
+  const onSetEnd = (time: string) => {
+    setEnd(time);
+  };
+  return {
+    modal,
+    onNextClick,
+    onCancel,
+    onConfirm,
+    start,
+    end,
+    onSetStart,
+    onSetEnd,
+  };
+}
+
+const FullScreen = styled.div`
+  position: fixed;
+  z-index: 30;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AskModalBlock = styled.div`
+  width: 400px;
+  background: white;
+  padding: 1.5rem;
+  border-radius: 4px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.125);
+  display: block;
+  h2 {
+    margin-top: 0.5rem;
+    margin-bottom: 1rem;
+  }
+  p {
+    margin-bottom: 3rem;
+  }
+  .buttons {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+const StyledButton = styled(Button)`
+  height: 2rem;
+  & + & {
+    margin-left: 0.75rem;
+  }
+`;
+
+export default function Modal({ visible, onCancel, onConfirm }: any) {
+  const { modal, onNextClick, start, end, onSetStart, onSetEnd } = useModal();
+  if (!visible) return null;
+  return (
+    <FullScreen>
+      <AskModalBlock>
+        {console.log(start, end)}
+        <UseHour onSetStart={onSetStart} onSetEnd={onSetEnd}></UseHour>
+        <h2>모임 설명</h2>
+        <p></p>
+        <div className="buttons">
+          <StyledButton onClick={onCancel}>취소</StyledButton>
+          <StyledButton onClick={onConfirm} to="/register">
+            확인
+          </StyledButton>
+        </div>
+      </AskModalBlock>
+    </FullScreen>
+  );
+}
