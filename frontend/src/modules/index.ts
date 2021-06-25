afecfda9502419ef2d21d40e80c53b3f9c7990c1
ww8007 <wshmin@naver.com>
2021-06-26 04:46:23 +0900

    [feat/front] add_indivisual, timetable_reducer 루트 리듀서에 time, indivisual 리듀서 추가

--- frontend/src/modules/index.ts
+++ frontend/src/modules/index.ts
@@ -1,11 +1,13 @@
-import { combineReducers } from "redux";
-import loading from "./loading";
-import sample, { sampleSaga } from "./sample";
-import calendar from "./calendar";
-import { all } from "redux-saga/effects";
-import user, { userSaga } from "./user";
-import auth, { authSaga } from "./auth";
-import time from "./time";
+import { combineReducers } from 'redux';
+import loading from './loading';
+import sample, { sampleSaga } from './sample';
+import calendar from './calendar';
+import { all } from 'redux-saga/effects';
+import user, { userSaga } from './user';
+import auth, { authSaga } from './auth';
+import timetable from './timetable';
+import time from './time';
+import individual from './individual';
 const rootReducer = combineReducers({
   loading,
   sample,
@@ -13,6 +15,8 @@ const rootReducer = combineReducers({
   user,
   auth,
   time,
+  timetable,
+  individual,
 });
 
 export default rootReducer;
