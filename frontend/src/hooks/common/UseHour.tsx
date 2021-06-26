3df6c6294732fb01bfe16b93cca62441b8e96d59
ww8007 <wshmin@naver.com>
2021-06-27 06:13:33 +0900

    [fix/front] fix_times_value

--- frontend/src/hooks/common/UseHour.tsx
+++ frontend/src/hooks/common/UseHour.tsx
@@ -2,35 +2,35 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Select from 'react-select';
 interface Props {
-  onSetStart: (time: string) => void;
-  onSetEnd: (time: string) => void;
+  onSetStart: (time: number) => void;
+  onSetEnd: (time: number) => void;
 }
 
 const times = [
-  { label: '오전 01:00', value: 'a1' },
-  { label: '오전 02:00', value: 'a2' },
-  { label: '오전 03:00', value: 'a3' },
-  { label: '오전 04:00', value: 'a4' },
-  { label: '오전 05:00', value: 'a5' },
-  { label: '오전 06:00', value: 'a6' },
-  { label: '오전 07:00', value: 'a7' },
-  { label: '오전 08:00', value: 'a8' },
-  { label: '오전 09:00', value: 'a9' },
-  { label: '오전 10:00', value: 'a10' },
-  { label: '오전 11:00', value: 'a11' },
-  { label: '오전 12:00', value: 'a12' },
-  { label: '오후 01:00', value: 'p1' },
-  { label: '오후 02:00', value: 'p2' },
-  { label: '오후 03:00', value: 'p3' },
-  { label: '오후 04:00', value: 'p4' },
-  { label: '오후 05:00', value: 'p5' },
-  { label: '오후 06:00', value: 'p6' },
-  { label: '오후 07:00', value: 'p7' },
-  { label: '오후 08:00', value: 'p8' },
-  { label: '오후 09:00', value: 'p9' },
-  { label: '오후 10:00', value: 'p10' },
-  { label: '오후 11:00', value: 'p11' },
-  { label: '오후 12:00', value: 'p12' },
+  { label: '오전 01:00', value: '1' },
+  { label: '오전 02:00', value: '2' },
+  { label: '오전 03:00', value: '3' },
+  { label: '오전 04:00', value: '4' },
+  { label: '오전 05:00', value: '5' },
+  { label: '오전 06:00', value: '6' },
+  { label: '오전 07:00', value: '7' },
+  { label: '오전 08:00', value: '8' },
+  { label: '오전 09:00', value: '9' },
+  { label: '오전 10:00', value: '10' },
+  { label: '오전 11:00', value: '11' },
+  { label: '오전 12:00', value: '12' },
+  { label: '오후 01:00', value: '13' },
+  { label: '오후 02:00', value: '14' },
+  { label: '오후 03:00', value: '15' },
+  { label: '오후 04:00', value: '16' },
+  { label: '오후 05:00', value: '17' },
+  { label: '오후 06:00', value: '18' },
+  { label: '오후 07:00', value: '19' },
+  { label: '오후 08:00', value: '20' },
+  { label: '오후 09:00', value: '21' },
+  { label: '오후 10:00', value: '22' },
+  { label: '오후 11:00', value: '23' },
+  { label: '오후 12:00', value: '24' },
 ];
 
 const TimeBlock = styled.div`
@@ -52,11 +52,11 @@ const TimeSelect = styled.div`
 
 const UseHour = ({ onSetStart, onSetEnd }: Props) => {
   const setStartHour = (e: any) => {
-    const time = e.value;
+    const time = Number(e.value);
     onSetStart(time);
   };
   const setEndHour = (e: any) => {
-    const time = e.value;
+    const time = Number(e.value);
     onSetEnd(time);
   };
   return (
@@ -64,12 +64,6 @@ const UseHour = ({ onSetStart, onSetEnd }: Props) => {
       <span>모임시간&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
       <TimeSelect>
         <Select
-          styles={{
-            menu: (provided) => ({
-              ...provided,
-              zIndex: 999,
-            }),
-          }}
           placeholder="선택"
           options={times}
           onChange={setStartHour}
@@ -78,12 +72,6 @@ const UseHour = ({ onSetStart, onSetEnd }: Props) => {
       <div>&nbsp;~&nbsp;</div>
       <TimeSelect>
         <Select
-          styles={{
-            menu: (provided) => ({
-              ...provided,
-              zIndex: 999,
-            }),
-          }}
           placeholder="선택"
           options={times}
           onChange={setEndHour}
