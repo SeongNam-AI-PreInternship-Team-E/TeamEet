ede9f4f15b4510f65f360a57f7a8293601b98ce2
ww8007 <wshmin@naver.com>
2021-06-26 20:36:00 +0900

    [feat/front] add_time, fix_PickWeek

--- frontend/src/modules/individual.ts
+++ frontend/src/modules/individual.ts
@@ -3,11 +3,15 @@ import lodash from 'lodash';
 type initial = {
   PickWeek: any;
   PickTime: any;
+  startHour: number;
+  endHour: number;
 };
 
 const initialState: initial = {
-  PickWeek: [],
+  PickWeek: [0, 1, 2, 3, 4, 5, 6, 7],
   PickTime: [],
+  startHour: 10,
+  endHour: 20,
 };
 
 export const individualSlice = createSlice({
@@ -15,15 +19,15 @@ export const individualSlice = createSlice({
   initialState,
   reducers: {
     cloneDays: (state, action: PayloadAction<any>) => {
-      state.PickWeek = lodash.cloneDeep(action.payload);
+      // state.PickWeek = lodash.cloneDeep(action.payload);
     },
     addTimes: (state) => {
-      for (let i = 0; i < 7; i++) {
+      for (let i = 0; i < 8; i++) {
         state.PickTime[i] = [];
       }
       if (state.PickTime) {
-        for (let j = 0; j < 7; j++) {
-          for (let i = 10; i <= 20; i += 0.5) {
+        for (let j = 0; j < 8; j++) {
+          for (let i = state.startHour; i <= state.endHour; i += 0.25) {
             if (state.PickTime[j]) {
               state.PickTime[j].push({ time: i });
             }
