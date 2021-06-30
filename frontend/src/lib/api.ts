9bfc124593fb18f2a54de3a329cff9e722680011
ww8007 <wshmin@naver.com>
2021-06-30 16:40:23 +0900

    [remove/front] remove_api

--- frontend/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getPost = (id: number) =>
-  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-export const getUsers = () =>
-  axios.get(`https://jsonplaceholder.typicode.com/users`);
