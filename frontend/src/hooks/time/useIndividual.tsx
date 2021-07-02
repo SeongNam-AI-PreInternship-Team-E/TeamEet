c981d6866b16a4bf2f52aff573f3275ca15a4f06
ww8007 <wshmin@naver.com>
2021-07-02 23:40:17 +0900

    [feat/front] add_button, cloneDates 팀페이지로 이동하는 버튼 생성 및 calendar_dates clone해오는 액션 dispatch

--- frontend/src/hooks/time/useIndividual.tsx
+++ frontend/src/hooks/time/useIndividual.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../modules';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { InPresent } from './InPresent';
 import { useDispatch } from 'react-redux';
 import { clickIndividualTime, dragTimes } from '../../modules/individual';
+import { useEffect } from 'react';
+import { cloneDates } from '../../modules/teamtime';
+import Button from '../../lib/styles/Button';
 
 export const useIndividual = () => {
-  const { PickWeek, PickTime } = useSelector((state: RootState) => ({
-    PickWeek: state.individual.PickWeek,
-    PickTime: state.individual.PickTime,
-  }));
+  const { PickWeek, PickTime, calendar_dates } = useSelector(
+    (state: RootState) => ({
+      PickWeek: state.individual.PickWeek,
+      PickTime: state.individual.PickTime,
+      calendar_dates: state.auth.calendar_dates,
+    })
+  );
   const [isDown, onChangeDown] = useState({
     drag: false,
     key: 9999,
   });
+  useEffect(() => {
+    dispatch(cloneDates(calendar_dates));
+  }, [calendar_dates]);
   const dispatch = useDispatch();
   const onClickDrag = (time: number, day: number) => {
     dispatch(dragTimes({ time, day }));
@@ -47,7 +56,7 @@ const Time = styled.div`
   text-align: center;
 `;
 
-const DayOfWeek = styled.div`
+const DayOfWeek = styled.div<{ back_color: string }>`
   margin-top: 3rem;
   font-size: 1.2rem;
   font-weight: bolder;
@@ -62,6 +71,14 @@ const DayOfWeek = styled.div`
   :nth-child(8) {
     color: blue;
   }
+  ${(props) =>
+    props.back_color === '#98A2FF'
+      ? css`
+          background-color: ${props.back_color};
+        `
+      : css`
+          opacity: 0.5;
+        `}
 `;
 export default function Individual() {
   const { PickWeek, PickTime, onClickDrag, isDown, onChangeDown, onClickTime } =
@@ -69,9 +86,14 @@ export default function Individual() {
   return (
     <TimeTableWrapper>
       <TimeTableContainer>
+        <Button to="/teamTable" middlewdith="true" cyan>
+          팀 시간표로
+        </Button>
         <Time>
           {PickWeek.map((day: any) => (
-            <DayOfWeek key={day.day}>{day.day}</DayOfWeek>
+            <DayOfWeek key={day.day} back_color={day.back_color}>
+              {day.day}
+            </DayOfWeek>
           ))}
 
           {PickTime.map((time: any, index: number) => (
