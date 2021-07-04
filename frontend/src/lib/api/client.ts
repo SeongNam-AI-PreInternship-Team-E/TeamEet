8b6ce424815722c6a74e1cf751f8ce1ef802e5cc
ww8007 <wshmin@naver.com>
2021-07-04 21:32:48 +0900

    [fix/front] fix_backend_url

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const client = axios.create();
 
-client.defaults.baseURL = 'http://52.79.248.172:8000/';
+client.defaults.baseURL = 'https://52.79.248.172:8000/';
 // client.defaults.baseURL = '';
 client.defaults.withCredentials = true;
 
