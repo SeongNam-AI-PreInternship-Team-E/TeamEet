81401f1edf08be0a3f915c840bd9f5a1da3ad9c9
Gnu-Kenny <qkr0677@gmail.com>
2021-08-09 11:15:56 +0900

    refact: 불필요 주석 삭제

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-mp4m&^+r36$507ucoyibbown0(@%h@fxvjm%gtl@+4k6((w%cs'
 SECRET_KEY = secret_key.SECRET_KEY
 ALGORITHM = os.environ.get("JWT_ALGORITHM")
 # SECURITY WARNING: don't run with debug turned on in production!
